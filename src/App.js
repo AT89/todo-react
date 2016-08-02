f5a472ad5f569b97ea9c99b2c359cdf053826a73
Anh Truong <anh.truong.07@cnu.edu>
2016-08-02 16:19:03 -0400

    initial commit, stuff showing

--- src/App.js
+++ src/App.js
@@ -1,1 +1,35 @@
 // Developer TODO: add App component
+import React, { Component } from 'react';
+import './App.css';
+
+class ToDo extends Component {
+
+render() {
+
+    return (
+      <div>
+      <h1>TO DO LIST</h1>
+
+      <div id="new">
+      </div>
+
+      <div id="notfin">
+      <h2>Unfinished</h2>
+        <div id="notfinitems">
+        <ul>{this.props.notfinitems}</ul>
+        </div>
+      </div>
+
+
+      <div id="fin">
+      <h2>Finished</h2>
+        <div id="finitems">
+        <ul>{this.props.finitems}</ul>
+        </div>
+      </div>
+      </div>
+    )
+
+  }
+}
+export default ToDo;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 // Developer TODO: add application entry point
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ToDo from './App';
+import './index.css';
+
+const toDoData = {
+  unfinished: ["Take out trash", "study ReactJS", "Study Aurelia", "get slick.js working"],
+  finished: ["poop"]
+}
+
+
+ReactDOM.render(
+  <ToDo
+  notfinitems={toDoData.unfinished}
+  finitems={toDoData.finished} />,
+  document.getElementById('root')
+);
