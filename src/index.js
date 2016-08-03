bd42968ed62e754d1cc4fae317d7f2fdb4dd6d59
Anh Truong <anh.truong.07@cnu.edu>
2016-08-03 10:01:43 -0400

    create works, single page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-// Developer TODO: add application entry point
-import React from 'react';
-import ReactDOM from 'react-dom'; //need this so Reat can manipulate the DOM
-import ToDo from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class NewItem extends React.Component {
+  constructor(props){
+    super()
+    this.state = props
+  }
+  render(){
+    return <form onSubmit={ e => this.create(e) }>
+      <input type='text' value={this.state.newItem} onChange={ e => this.change(e) } />
+    </form>
+  }
+  change(e){
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+  create(e){
+    e.preventDefault()
+    // add this below to existing array
+    this.props.onCreate(this.state.newItem)
+    this.setState({newItem: ''})
+  }
+}
+
+class List extends React.Component {
+  constructor(props){
+    super()
+    this.state = props
+  }
+  addItem(text){
+    let items = this.state.items
+    items.push(text)
+    this.setState({items})
+  }
+
+  render(){
+    return <div>
+      <NewItem newItem='' onCreate={ text => this.addItem(text) }/>
+      <ul>
+        {this.props.items.map( (item, index) => {
+          return <li key={index}>{item}</li>
+        })}
+      </ul>
+    </div>
+  }
+}
 
 let items = [
-  "Learn react",
-  "Get Mix-express working",
-  "Learn computer sci"
+  "learn react",
+  "learn comp sci"
 ]
-
 ReactDOM.render(
-  <ToDo
-  items={items} />,
-  document.getElementById('root') //can use this multiple times to point to diff parts of the file or 1 part specifically, like a div
-);
+  <List items={items}/>,
+  document.getElementById('root')
+)
